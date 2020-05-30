8e33b97149c27852b544f043be64d60dfdd98951
cmarques20101 <cmarques20101@hotmail.com>
2020-05-30 21:58:56 +0100

    1 file modified, 2 added

---
+++ src/app/service-edit/service-edit.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { TServico } from '../files/tservico';
+
+@Component({
+  selector: 'app-service-edit',
+  templateUrl: './service-edit.component.html',
+  styleUrls: ['./service-edit.component.css']
+})
+export class ServiceEditComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
