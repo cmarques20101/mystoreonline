fd10941747a4dbf41c7b80fe318c2672a350240c
cmarques20101 <cmarques20101@hotmail.com>
2020-05-30 07:34:02 +0100

    1 file added

---
+++ src/app/app-routing/app-routing.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { ServicoListComponent } from '../servico-list/servico-list.component';
+
+const routes: Routes = [
+  { path: 'servicos', component: ServicoListComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
+
+
+/*
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: []
+})
+export class AppRoutingModule { }
+*/
