12f18559925ce9aea2dc2471e30493207eb4375a
cmarques20101 <cmarques20101@hotmail.com>
2020-05-31 18:25:42 +0100

    2 files modified

--- src/app/app-routing/app-routing.module.ts
+++ src/app/app-routing/app-routing.module.ts
@@ -3,9 +3,8 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { DashboardComponent }   from '../dashboard/dashboard.component';
 
-import { ServicoViewComponent }  from '../servico-view/servico-view.component';
-
 import { ServicoListComponent } from '../servico-list/servico-list.component';
+import { ServicoViewComponent }  from '../servico-view/servico-view.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
@@ -21,17 +20,3 @@ const routes: Routes = [
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
-
-
-/*
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-
-@NgModule({
-  imports: [
-    CommonModule
-  ],
-  declarations: []
-})
-export class AppRoutingModule { }
-*/
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,59 +7,70 @@ import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 
-import { HelloxComponent } from './hellox/hellox.component';
-import { HelloyComponent } from './helloy/helloy.component';
+  import { HelloxComponent } from './hellox/hellox.component';
+  import { HelloyComponent } from './helloy/helloy.component';
 
-import { TopBarComponent } from './top-bar/top-bar.component';
+  import { TopBarComponent } from './top-bar/top-bar.component';
 
-import { ProductListComponent } from './product-list/product-list.component';
-import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
-import { ProductAlertsNotComponent } from './product-alerts-not/product-alerts-not.component';
+  import { ProductListComponent } from './product-list/product-list.component';
+  import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
+  import { ProductAlertsNotComponent } from './product-alerts-not/product-alerts-not.component';
 
+
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ServicoViewComponent } from './servico-view/servico-view.component';
 import { ServicoListComponent } from './servico-list/servico-list.component';
+
 import { ServicoListItemComponent } from './servico-list-item/servico-list-item.component';
 
 import { ServicoEditComponent } from './servico-edit/servico-edit.component';
-import { ServicoViewComponent } from './servico-view/servico-view.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
+
+
 import { ServicoService } from './servico.service';
 import { MessagesComponent } from './messages/messages.component';
 import { MessageService } from './message.service';
 
 
+import { AppRoutingModule }     from './app-routing/app-routing.module';
+
 @NgModule({
   imports: [
     BrowserModule,
-      FormsModule,
-
-    ReactiveFormsModule,
-    /**/
+    FormsModule,
+    AppRoutingModule
+    //ReactiveFormsModule,
+    
+    /* 
     RouterModule.forRoot([
       //{ path: '', component: ProductListComponent },
       { path: '', component: ServicoListComponent },
-    /**/
-    ])
+    */
+    
   ],
   declarations: [
     AppComponent,
     
-    TopBarComponent,
+      TopBarComponent,
     
-    HelloyComponent
-    , HelloxComponent,
+      HelloyComponent
+      , HelloxComponent,
 
-    ProductListComponent,
-    ProductAlertsComponent,
-    ProductAlertsNotComponent,
-    
+      ProductListComponent,
+      ProductAlertsComponent,
+      ProductAlertsNotComponent,
+      
     ServicoListComponent
     , ServicoListItemComponent
     , ServicoEditComponent
-    , ServicoViewComponent, DashboardComponent, MessagesComponent
+    , ServicoViewComponent
+    
+    , DashboardComponent
+    , MessagesComponent
     
   ],
-  bootstrap: [ AppComponent ],
-  providers: [ServicoService, MessageService]
+  bootstrap: [ AppComponent ]
+  //,
+  //providers: [ServicoService, MessageService]
 })
 export class AppModule { }
 
