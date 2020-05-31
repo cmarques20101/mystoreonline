af2ec84bb35b49fd5b9157c5b8e4c20bdcae2a12
cmarques20101 <cmarques20101@hotmail.com>
2020-05-31 23:49:11 +0100

    1 file modified

--- src/app/servico.service.ts
+++ src/app/servico.service.ts
@@ -5,6 +5,7 @@ import { Observable, of } from 'rxjs';
 import { Servico } from './servico';
 import { servicos } from './servicos';
 import { MessageService } from './message.service';
+import { QUOTED_KEYS } from '@angular/compiler/src/output/value_util';
 
 @Injectable({
   providedIn: 'root',
@@ -13,6 +14,10 @@ import { MessageService } from './message.service';
 export class ServicoService {
 
   constructor(private messageService: MessageService) { }
+
+  log(message: string) {
+    this.messageService.add(`ServicoService: ${message}`);
+  }
 
   /*
   getServicos(): Servico[] {
@@ -23,6 +28,26 @@ export class ServicoService {
     this.messageService.add('ServicoService: fetched servicos all!');
     return of(servicos);
   }
+  getServicosCount(term: string): number {
+    let qtd: number = 0;
+    const source = of(servicos);
+    const myqtd = source.subscribe(val => qtd = val.length);
+    
+    this.messageService.add('ServicoService: fetched servicos Count '+qtd+'!');
+    return qtd;
+  }
+  /*
+    searchHeroesCount(term: string): number {
+      let c = 0;
+      this.log(`searchHeroesCount: !`);
+      //let heroes =  searchHeroes(term);
+      const subscribe =  this.searchHeroes(term).subscribe(val => c = val.length );
+      //const subscribe =  this.heroes$.subscribe(val => this.searchTermsCount = val.length );
+      this.log('searchHeroesCount: #'+c+'!');
+    return c;
+    }
+
+  */
 
   getServico(id: number): Observable<Servico> {
     // TODO: send the message _after_ fetching the hero
