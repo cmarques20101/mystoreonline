5e1943b962faecca9ce5c4fc3addf092a224b14e
cmarques20101 <cmarques20101@hotmail.com>
2020-06-02 22:09:47 +0100

    11 ups

--- src/app/message.service.ts
+++ src/app/message.service.ts
@@ -18,7 +18,7 @@ export class MessageService {
     //this.messages = [];
     this.messages.push(message);
     if (this.messages.length >3) {
-      this.messages = this.messages.slice(this.messages.length-1-3,this.messages.length-1);
+      this.messages = this.messages.slice(this.messages.length-1-,this.messages.length);
     }
 
   }
