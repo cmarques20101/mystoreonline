63a3a876446622cc842239879b92c43a6cb99735
cmarques20101 <cmarques20101@hotmail.com>
2020-06-02 15:13:05 +0100

    2 files modified

--- src/app/app-routing/app-routing.module.ts
+++ src/app/app-routing/app-routing.module.ts
@@ -6,15 +6,17 @@ import { DashboardComponent }   from '../dashboard/dashboard.component';
 import { ServicoListComponent } from '../servico-list/servico-list.component';
 import { ServicoViewComponent }  from '../servico-view/servico-view.component';
 
+import { ProductComponent }  from '../product-list/product-list.component';
+
 const routes: Routes = [
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
   { path: 'dashboard', component: DashboardComponent },
   { path: 'app-servico-view/:id', component: ServicoViewComponent },
   { path: 'app-servico-list', component: ServicoListComponent }
-  
+  { path: 'app-product-list', component: ProductComponent }
 
 ];
-
+//app-product-list
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
