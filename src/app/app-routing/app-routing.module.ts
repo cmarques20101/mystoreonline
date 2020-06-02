f5f6294f9a5652e314fff1a8dade2b76d0feef99
cmarques20101 <cmarques20101@hotmail.com>
2020-06-02 15:14:08 +0100

    5 ups

--- src/app/app-routing/app-routing.module.ts
+++ src/app/app-routing/app-routing.module.ts
@@ -6,14 +6,14 @@ import { DashboardComponent }   from '../dashboard/dashboard.component';
 import { ServicoListComponent } from '../servico-list/servico-list.component';
 import { ServicoViewComponent }  from '../servico-view/servico-view.component';
 
-import { ProductComponent }  from '../product-list/product-list.component';
+import { ProductListComponent }  from '../product-list/product-list.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
   { path: 'dashboard', component: DashboardComponent },
   { path: 'app-servico-view/:id', component: ServicoViewComponent },
   { path: 'app-servico-list', component: ServicoListComponent }
-  ,{ path: 'app-product-list', component: ProductComponent }
+  ,{ path: 'app-product-list', component: ProductLComponent }
 
 ];
 //app-product-list
