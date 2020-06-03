228ae8a8072ea2cbcc55630ea6e35d3f55990512
cmarques20101 <cmarques20101@hotmail.com>
2020-06-03 16:46:53 +0100

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,7 +9,7 @@ import { Input } from '@angular/core';
 export class AppComponent  {
   nameApp = 'Angular ' + VERSION.major + "";
   //@Input titlex; // = 'my store online 2';
-  title = 'my store online 2';
+  title = 'Serviços Técnicos';
 }
 
 
